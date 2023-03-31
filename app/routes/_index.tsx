b0bf3b4bae5072e0d30879be3b6be715154b76bd
fonsecaBarreto <lucasbfonte@gmail.com>
2023-03-31 20:30:48 -0300

    initial

---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+    </div>
+  );
+}
