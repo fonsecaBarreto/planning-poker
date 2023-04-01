a5826c772f5db4304a8da41571221a646dbb0eb8
fonsecaBarreto <lucasbfonte@gmail.com>
2023-04-01 18:08:28 -0300

    feat: implement prisma

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,33 +1,15 @@
-import fs from "fs";
-import path from "path";
 
-const file_name = path.resolve(__dirname,'users.json')
-    
+import { db } from "~/utils/db.server";
+
 export function findManyUsers(/* userIds: string[] */): any{
-  const { users }  = (require(file_name));
-  const result = users;
-  //  users.filter((u: any) => userIds.includes(u.id));
-  return result;
+  return [];
 }
 
 export function getUserById(userId: string): any{
-  const { users }  = (require(file_name));
-  const user = users.find((u: any) => u.id === userId);
-  return user;
+  return db.user.findFirst({ where: { id: userId} });
 }
 
-export async function createUser({ nickName }: any) {
-  const { users }  = (require(file_name));
-  const user = { id: String(users.length + 1), nickName };
-  fs.writeFileSync(file_name, JSON.stringify({ users: [ ...users, user ] }, null, 4));
-  return user;
-}
-
-export async function updateUser( userId: string, { nickName }: any) {
-  const { users }: { users: any[]}  = (require(file_name));
-  const user = getUserById(userId);
-  const new_list = users.splice( users.findIndex(u=>u.id === userId), 1, { ...user, nickName})
-  fs.writeFileSync(file_name, JSON.stringify(new_list, null, 4));
-  return;
+export function createUser({ nickName }: any) {
+  return db.user.create({ data: { nickName } });
 }
 
