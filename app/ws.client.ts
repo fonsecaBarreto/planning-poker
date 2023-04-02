e5ca2130ce0b680b4e15e1a7cd58652b40fb6d35
fonsecaBarreto <lucasbfonte@gmail.com>
2023-04-02 00:33:27 -0300

    test deploy

--- app/ws.client.ts
+++ app/ws.client.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-const URL = 'http://localhost:3000';
-// const URL ='https://lol-planning-poker.herokuapp.com'
+// const URL = 'http://localhost:3000';
+const URL ='https://lol-planning-poker.herokuapp.com'
 
 export function connect() {
   return io(URL);
